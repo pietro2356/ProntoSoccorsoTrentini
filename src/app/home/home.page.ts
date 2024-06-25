14af1d7c3b604ed03e3d0fc3306e4e1eafd5bd4a
Pietro Rocchio <pietro.rocchio@gpi.it>
2024-06-25 22:26:03 +0200

    Creata prova app principale

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, signal } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonMenu,
+  IonButtons,
+  IonMenuButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonFooter,
+} from '@ionic/angular/standalone';
+import { StatoProntoSoccorso } from '@core/models/statoProntoSoccorso';
+import { StatoPSService } from '@core/services/StatoPS/stato-ps.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonMenu,
+    IonButtons,
+    IonMenuButton,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardContent,
+    IonFooter,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  #statoPSService = inject(StatoPSService);
+  statoPS = signal<StatoProntoSoccorso>({} as StatoProntoSoccorso);
+
+  constructor() {
+    this.#statoPSService.getStatoPS().subscribe(statoPS => {
+      this.statoPS.set(statoPS as StatoProntoSoccorso);
+    });
+  }
 }
