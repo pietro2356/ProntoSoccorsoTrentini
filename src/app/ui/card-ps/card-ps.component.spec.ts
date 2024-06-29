7e1979739bec3943e9ad9704279610e398cf7ffa
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-06-29 11:45:36 +0200

    Creata Card visualizzazione Stato Pronto Soccorso

---
+++ src/app/ui/card-ps/card-ps.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { CardPSComponent } from './card-ps.component';
+
+describe('CardPSComponent', () => {
+  let component: CardPSComponent;
+  let fixture: ComponentFixture<CardPSComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CardPSComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CardPSComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
