c154a55efb4b39041e4f362e247b235906abca5a
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-06-30 19:12:11 +0200

    Migliorato e ottimizzato il componente principale app.component.ts. Rimossi template HTML e CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonRouterOutlet,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
+  template: '<ion-app><ion-router-outlet/></ion-app>',
   standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
