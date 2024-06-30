dbe8396e3e2e0152eb830bee8c45be3040e2d89a
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-06-30 19:13:21 +0200

    Create Pipe per la corretta visualizzazione dei dati delle card sui Pronto Soccorso

--- src/app/core/pipe/extract-ps.pipe.ts
+++ src/app/core/pipe/extract-ps.pipe.ts
@@ -7,9 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ExtractPSPipe implements PipeTransform {
 
   transform(value: string): unknown {
-    const prontoSoccorso = value.split('-')[1];
-    console.log({ prontoSoccorso });
-    return prontoSoccorso;
+    return value.split('-')[1];
   }
 
 }
--- src/app/core/pipe/first-letter-to-capital.pipe.ts
+++ src/app/core/pipe/first-letter-to-capital.pipe.ts
@@ -9,10 +9,7 @@ export class FirstLetterToCapitalPipe implements PipeTransform {
   transform(value: string): string {
     if (!value) return '';
     const textLower = value.toLowerCase();
-    const textFirstupper = textLower.charAt(0).toUpperCase() + textLower.slice(1);
-    console.log({ textFirstupper });
-    return textFirstupper
-
+    return textLower.charAt(0).toUpperCase() + textLower.slice(1)
   }
 
 }
