f49032e3e71936ee45d587b74283729091b4186e
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-06-30 19:34:35 +0200

    Creato servizio per la gestione dello stato dell'app

---
+++ src/app/core/services/appState/app-state.service.ts
@@ -0,0 +1,51 @@
+import { computed, Injectable, signal } from '@angular/core';
+
+/**
+ * Stato: READY | LOADING | ERROR
+ */
+
+export type TState = 'READY' | 'LOADING' | 'ERROR';
+export interface IState {
+  state: TState;
+  error: string | null;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppStateService {
+
+  #appState = signal<IState>({ state: 'READY', error: null });
+
+  public appState = computed<IState>(() => {
+    return this.#appState();
+  });
+
+  public get state(): TState {
+    return this.#appState().state;
+  }
+
+  public setReady(): void {
+    this.#appState.update(() => ({ state: 'READY', error: null }));
+  }
+
+  public setLoading(): void {
+    this.#appState.update(() => ({ state: 'LOADING', error: null }));
+  }
+
+  public setError(error: string): void {
+    this.#appState.update(() => ({ state: 'ERROR', error }));
+  }
+
+  public isStatusLoading(): boolean {
+    return this.#appState().state === 'LOADING';
+  }
+
+  public isStatusError(): boolean {
+    return this.#appState().state === 'ERROR';
+  }
+
+  public isStatusReady(): boolean {
+    return this.#appState().state === 'READY';
+  }
+}
