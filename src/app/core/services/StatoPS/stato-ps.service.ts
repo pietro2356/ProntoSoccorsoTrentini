39e78d8ebb3fdeed45d3709084c68be7af7bf6c7
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-07-06 14:34:30 +0200

    Sistemato tipo di dato servizio statops

--- src/app/core/services/StatoPS/stato-ps.service.ts
+++ src/app/core/services/StatoPS/stato-ps.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable} from '@angular/core';
 import { HttpCoreService } from '@core/services/http/http-core.service';
 import { Observable } from 'rxjs';
 import { environment } from '@env/environment';
+import { StatoProntoSoccorso } from '@core/models/statoProntoSoccorso';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class StatoPSService {
 
   #httpCore = inject(HttpCoreService);
 
-  public getStatoPS<StatoProntoSoccorso>(): Observable<StatoProntoSoccorso> {
+  public getStatoPS(): Observable<StatoProntoSoccorso> {
     return this.#httpCore.get<StatoProntoSoccorso>(environment.apiTrentinoAA);
   }
 }
