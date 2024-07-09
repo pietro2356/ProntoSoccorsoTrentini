a0fc4cde268db6b2ed85be7277e636e5b7fb819a
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-07-09 19:13:19 +0200

    Corretto identificativo APP

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'pr.prontosoccorso.trentini',
   appName: 'ProntoSoccorsoTrentini',
   webDir: 'www'
 };
