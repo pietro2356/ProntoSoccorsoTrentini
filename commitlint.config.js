f1784aa800d418cd758d7c3fcf2003c82e7337b3
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-07-16 19:16:40 +0200

    fix(lint): add commit lint rule
    Added rule on commit linting configuration to increment the max number of character in body section of commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-angular', '@commitlint/config-conventional'],
+  rules: {
+    'body-max-length': [0, 'always', 200],
+    'body-max-line-length': [0, 'always', 200],
+  },
 };
