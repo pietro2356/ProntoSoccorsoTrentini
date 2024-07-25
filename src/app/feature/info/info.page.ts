b834ba07e63a953c45c3f7604f64635379dfaf62
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-07-25 02:27:12 +0200

    fix(info-page): corrected info page btn

--- src/app/feature/info/info.page.ts
+++ src/app/feature/info/info.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 import { LinkButtonComponent } from '@ui/link-button/link-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'pst-info',
   templateUrl: './info.page.html',
   styleUrls: ['./info.page.scss'],
   standalone: true,
-  imports: [IonicModule, RouterLinkActive, RouterLink, LinkButtonComponent],
+  imports: [IonicModule, LinkButtonComponent, RouterLink],
 })
 export class InfoPage {}
