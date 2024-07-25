770327439a3777daefb6892b8fabfef51ed7ee26
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-07-25 12:03:46 +0200

    fix(not-found): added new link button to page

--- src/app/feature/not-found/not-found-page.component.ts
+++ src/app/feature/not-found/not-found-page.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { LinkButtonComponent } from '@ui/link-button/link-button.component';
 
 @Component({
   selector: 'pst-not-found',
   standalone: true,
-  imports: [RouterLink, IonContent, IonHeader, IonTitle, IonToolbar],
+  imports: [RouterLink, IonContent, IonHeader, IonTitle, IonToolbar, LinkButtonComponent],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
