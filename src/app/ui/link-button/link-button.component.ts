a4c5a61a314e81fec8de9e938516f31ad1357ec0
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-07-25 12:02:04 +0200

    fix(link-btn): changed core button
    Changed core button from Ionic to PrimeNG

--- src/app/ui/link-button/link-button.component.ts
+++ src/app/ui/link-button/link-button.component.ts
@@ -1,28 +1,31 @@
 import { Component, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { ButtonModule } from 'primeng/button';
 
-type ActionButtonColor =
-  | 'primary'
-  | 'secondary'
-  | 'tertiary'
+// type Severity = 'success' | 'info' | 'warn' | 'error' | 'help' | 'danger' | 'contrast';
+type Severity =
   | 'success'
+  | 'info'
   | 'warning'
   | 'danger'
-  | 'light'
-  | 'medium'
-  | 'dark';
+  | 'help'
+  | 'primary'
+  | 'secondary'
+  | 'contrast'
+  | null
+  | undefined;
 
 @Component({
   selector: 'pst-link-button',
   templateUrl: './link-button.component.html',
   styleUrls: ['./link-button.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, IonicModule],
+  imports: [RouterLink, RouterLinkActive, IonicModule, ButtonModule],
 })
 export class LinkButtonComponent {
   icon = input<string>();
   text = input.required<string>();
-  link = input.required<string>();
-  color = input<ActionButtonColor>();
+  link = input<string>();
+  severity = input<Severity>();
 }
