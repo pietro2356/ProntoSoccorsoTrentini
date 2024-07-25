ad4cc3c0167ff222599c2a103ef89b6d26aedbe1
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-07-25 12:01:13 +0200

    fix(info-page): added ionicContent tag to page root

--- src/app/feature/info/info.page.ts
+++ src/app/feature/info/info.page.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LinkButtonComponent } from '@ui/link-button/link-button.component';
 import { RouterLink } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'pst-info',
   templateUrl: './info.page.html',
   styleUrls: ['./info.page.scss'],
   standalone: true,
-  imports: [IonicModule, LinkButtonComponent, RouterLink],
+  imports: [LinkButtonComponent, RouterLink, IonContent, IonHeader, IonTitle, IonToolbar],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoPage {}
