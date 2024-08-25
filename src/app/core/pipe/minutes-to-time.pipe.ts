90dfa6bc283dc5b4f81b26959237232a528123d3
Pietro Rocchio <pietro.rocchio@gmail.com>
2024-08-25 23:58:51 +0200

    chore(pipe): create minute to time
    Create a pipe that take the value of time in minute and return it in time

---
+++ src/app/core/pipe/minutes-to-time.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'minutesToTime',
+  standalone: true,
+})
+export class MinutesToTimePipe implements PipeTransform {
+  transform(minutes: number): string {
+    if (minutes === undefined || minutes === null) {
+      return '';
+    }
+    if (minutes === 0) return '0min';
+
+    const hours = Math.floor(minutes / 60);
+    const minutesRemaining = minutes % 60;
+
+    let formattedTime = '';
+    if (hours > 0) {
+      formattedTime += hours + 'h ';
+    }
+    if (minutesRemaining > 0) {
+      formattedTime += minutesRemaining + 'min';
+    }
+
+    return formattedTime;
+  }
+}
