15446d0be5b1cb81e5704f49e96fbfdffc5ba66b
Pietro Rocchio <pietro.rocchio@gpi.it>
2024-10-23 08:33:05 +0200

    chore(i18n): :globe_with_meridians: created pipe to extract PS code
    This Pipe covert textual type of PS into corresponding translation code

--- src/app/core/pipe/extract-ps.pipe.spec.ts
+++ src/app/core/pipe/extract-ps.pipe.spec.ts
@@ -8,11 +8,11 @@ describe('ExtractPSPipe', () => {
 
   it('should return the PS Type', () => {
     const pipe = new ExtractPSPipe();
-    expect(pipe.transform('Ospedale di Cles - Pronto Soccorso')).toBe('Pronto Soccorso');
+    expect(pipe.transform('Ospedale di Cles - Pronto Soccorso')).toBe('homeStatoPS.label.ps');
   });
 
   it('should return the PS Type', () => {
     const pipe = new ExtractPSPipe();
-    expect(pipe.transform('Ospedale di Trento - Pronto Soccorso Pediatrico')).toBe('Pronto Soccorso Pediatrico');
+    expect(pipe.transform('Ospedale di Trento - Pronto Soccorso Pediatrico')).toBe('homeStatoPS.label.ps_pediatric');
   });
 });
--- src/app/core/pipe/extract-ps.pipe.ts
+++ src/app/core/pipe/extract-ps.pipe.ts
@@ -5,7 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class ExtractPSPipe implements PipeTransform {
-  transform(value: string): unknown {
-    return value.split(' - ')[1];
+  transform(value: string): string {
+    const psValue = value.split(' - ')[1];
+    const psTypeLen = psValue.split(' ');
+    if (psTypeLen.length === 2) return 'homeStatoPS.label.ps';
+
+    switch (psTypeLen[2]) {
+      case 'Pediatrico':
+        return 'homeStatoPS.label.ps_pediatric';
+      case 'Ortopedico':
+        return 'homeStatoPS.label.ps_orthopedic';
+      case 'Ginecologico':
+        return 'homeStatoPS.label.ps_ginecologic';
+      case 'Oculistico':
+        return 'homeStatoPS.label.ps_oculistic';
+      default:
+        return 'homeStatoPS.label.ps';
+    }
   }
 }
