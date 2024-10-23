8f16dd881cc58f36af6279068c4c9e51827de013
Pietro Rocchio <pietro.rocchio@gpi.it>
2024-10-23 08:40:07 +0200

    chore(i18n): :globe_with_meridians: added supported language to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,23 @@
 export const environment = {
   production: false,
   apiTrentinoAA: 'https://trec.trentinosalute.net/api/opendata-statops/v2/opendata/statops/statops',
+  supportedLanguages: [
+    {
+      code: 'it',
+      label: 'Italiano',
+      flag: '🇮🇹',
+    },
+    {
+      code: 'en',
+      label: 'English',
+      flag: '🇬🇧',
+    },
+    {
+      code: 'de',
+      label: 'Deutsch',
+      flag: '🇩🇪',
+    },
+  ],
 };
 
 /*
