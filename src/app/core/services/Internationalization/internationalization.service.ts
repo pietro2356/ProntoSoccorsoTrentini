e8914499b1cc425e543c301de4f07b970eb68a0f
Pietro Rocchio <pietro.rocchio@gpi.it>
2024-10-23 08:43:46 +0200

    docs(i18n): :globe_with_meridians: documented i18n service

--- src/app/core/services/Internationalization/internationalization.service.ts
+++ src/app/core/services/Internationalization/internationalization.service.ts
@@ -3,15 +3,33 @@ import { TranslateService } from '@codeandweb/ngx-translate';
 import { TLang } from '@core/models/TLang';
 import { environment } from '@env/environment';
 
+/**
+ * @description Servizio per la gestione dell'internazionalizzazione dell'applicazione
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class InternationalizationService {
+  /**
+   * @description Servizio per la traduzione delle stringhe fornito dalla libreria ngx-translate
+   * @private
+   */
   readonly #translateService = inject(TranslateService);
 
+  /**
+   * @description Signal contenente la lista delle lingue `TLang` supportate dall'applicazione lette dall'environment
+   * @private
+   */
   readonly #supportedLanguages = signal<TLang[]>(environment.supportedLanguages);
+
+  /**
+   * @description Signal contenente le bamdere delle lingue supportate dall'applicazione
+   */
   public availLangFlag = computed<string[]>(() => this.#supportedLanguages().map(lang => lang.flag));
 
+  /**
+   * @description Signal contenente la bandiera della lingua selezionata
+   */
   selectedLanguageFlag = computed(() => {
     const selectedLangCode = this.#translateService.currentLang.toUpperCase();
     const selectedLang = this.#supportedLanguages().find(lang => lang.code.toUpperCase() === selectedLangCode);
@@ -20,10 +38,18 @@ export class InternationalizationService {
 
   constructor() {}
 
+  /**
+   * @description Cambia la lingua dell'applicazione in base al codice della lingua come: `en`, `it`, ecc.
+   * @param langCode Codice della lingua
+   */
   public changeLanguageWithCode(langCode: string): void {
     this.#translateService.use(langCode);
   }
 
+  /**
+   * @description Cambia la lingua dell'applicazione in base alla bandiera della lingua come: `🇮🇹`, `🇬🇧`, ecc.
+   * @param langFlag Bandiera della lingua
+   */
   public changeLanguageWithFlag(langFlag: string): void {
     const selectedLang = this.#supportedLanguages().find(lang => lang.flag === langFlag);
     if (selectedLang) {
@@ -31,14 +57,11 @@ export class InternationalizationService {
     }
   }
 
+  /**
+   * @description Inizializza la lingua corrente con la lingua di default.
+   * Se la lingua del browser non è supportata, viene utilizzata la lingua di default.
+   */
   public initCurrentLangFromDefaultLang(): void {
     this.changeLanguageWithCode(this.#translateService.getBrowserLang() ?? this.#translateService.defaultLang);
-  }
-
-  public logInfo(): void {
-    console.log('Supported Lang: ', this.#supportedLanguages());
-    console.log('Current Lang: ', this.#translateService.currentLang);
-    console.log('Default Lang: ', this.#translateService.defaultLang);
-    console.log('Browser Lang: ', this.#translateService.getBrowserLang());
   }
 }
