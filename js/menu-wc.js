5b6e47ef57240bfedf6378bdb22cf2d6a30d7e43
pietro2356 <32908126+pietro2356@users.noreply.github.com>
2024-10-29 15:30:23 +0000

    Deploying to gh-pages from @ pietro2356/ProntoSoccorsoTrentini@0c97b08fd443eb8043e8c278954b1fdb87e960c6 🚀

---
+++ js/menu-wc.js
@@ -0,0 +1,216 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">pronto-soccorso-trentini documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search"><input type="text" placeholder="Type to search"></div>` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                        <li class="link">
+                            <a href="overview.html" data-type="chapter-link">
+                                <span class="icon ion-ios-keypad"></span>Overview
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="index.html" data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>README
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="changelog.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>CHANGELOG
+                            </a>
+                        </li>
+                        <li class="link">
+                            <a href="license.html"  data-type="chapter-link">
+                                <span class="icon ion-ios-paper"></span>LICENSE
+                            </a>
+                        </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CardPSComponent.html" data-type="entity-link" >CardPSComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CardpsLoaderComponent.html" data-type="entity-link" >CardpsLoaderComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ErrorCardComponent.html" data-type="entity-link" >ErrorCardComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FavoritesPage.html" data-type="entity-link" >FavoritesPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HomeStatoPSPage.html" data-type="entity-link" >HomeStatoPSPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/InfoPage.html" data-type="entity-link" >InfoPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/LinkButtonComponent.html" data-type="entity-link" >LinkButtonComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/NotFoundPage.html" data-type="entity-link" >NotFoundPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/PsDetailsPage.html" data-type="entity-link" >PsDetailsPage</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/WaitingBoxComponent.html" data-type="entity-link" >WaitingBoxComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#classes-links"' :
+                            'data-bs-target="#xs-classes-links"' }>
+                            <span class="icon ion-ios-paper"></span>
+                            <span>Classes</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="classes-links"' : 'id="xs-classes-links"' }>
+                            <li class="link">
+                                <a href="classes/FavoriteUtils.html" data-type="entity-link" >FavoriteUtils</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/AppStateService.html" data-type="entity-link" >AppStateService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/FavoritesService.html" data-type="entity-link" >FavoritesService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/GlobalErrorHandler.html" data-type="entity-link" >GlobalErrorHandler</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/HttpCoreService.html" data-type="entity-link" >HttpCoreService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/InternationalizationService.html" data-type="entity-link" >InternationalizationService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/LocalStoreService.html" data-type="entity-link" >LocalStoreService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/StatoPSService.html" data-type="entity-link" >StatoPSService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/Color.html" data-type="entity-link" >Color</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/CoreOptions.html" data-type="entity-link" >CoreOptions</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/IState.html" data-type="entity-link" >IState</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#pipes-links"' :
+                                'data-bs-target="#xs-pipes-links"' }>
+                                <span class="icon ion-md-add"></span>
+                                <span>Pipes</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="pipes-links"' : 'id="xs-pipes-links"' }>
+                                <li class="link">
+                                    <a href="pipes/ExtractPSPipe.html" data-type="entity-link" >ExtractPSPipe</a>
+                                </li>
+                                <li class="link">
+                                    <a href="pipes/FirstLetterToCapitalPipe.html" data-type="entity-link" >FirstLetterToCapitalPipe</a>
+                                </li>
+                                <li class="link">
+                                    <a href="pipes/MinutesToTimePipe.html" data-type="entity-link" >MinutesToTimePipe</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/typealiases.html" data-type="entity-link">Type aliases</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
